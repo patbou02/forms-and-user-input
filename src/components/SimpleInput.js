440a2e1b75291fa25f3b36edfb9243d9107192e1
Patrick Boutin <patrick.boutin@roberthalf.com>
2022-11-14 00:51:14 -0500

    Use useState to stroe entered name in input field on change while preventing form submit via preventDefault().

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,20 @@
+import { useState, useRef } from 'react';
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputChangeHandler = e => setEnteredName(e.target.value);
+
+  const formSubmitHandler = e => {
+    e.preventDefault();
+    console.log(enteredName);
+  }
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={nameInputChangeHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
