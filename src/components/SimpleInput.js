6c4db9186279f63145a1aaf99abeceb95259978a
Patrick Boutin <patrick.boutin@roberthalf.com>
2022-11-15 21:10:36 -0500

    Refactored form validation logic to not use useEffect since in this case it is overkill and can easily be performed without.

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,27 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('');
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-  const [formIsValid, setFormIsValid] = useState(false);
 
   const enteredNameIsValid = enteredName.trim() !== '';
   // if we had more fields, we'd have additional variables such as enteredAgeIsValid.
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
-  useEffect(() => {
-    /* if there were more fields we'd have more states as in #1 above, and then we'd do:
-      if (enteredNameIsValid && enteredAgeIsValid) {...} else {...} where we'd
-      set formIsValid to TRUE if all the dependencies are true otherwise we'd set it to FALSE.
-    */
-    if (enteredNameIsValid) {
-      setFormIsValid(true);
-    } else {
-      setFormIsValid(false);
-    }
-  }, [enteredNameIsValid]);
-  // we'd list all the dependencies in this array [enteredNameIsValid, enteredAgeIsValid]
-  
+  let formIsValid = false;
+
+  /* if there were more fields we'd have more states as in #1 above, and then we'd do:
+    if (enteredNameIsValid && enteredAgeIsValid) {...} else {...} where we'd
+    set formIsValid to TRUE if all the dependencies are true otherwise we'd set it to FALSE.
+  */
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
 
   const enteredNameChangeHandler = e => {
     setEnteredName(e.target.value);
