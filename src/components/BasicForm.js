a5952a6addf605bec19a273dac8c7e21fcfb853c
Patrick Boutin <patrick.boutin@roberthalf.com>
2022-11-23 11:29:14 -0800

    Challenge: switched form to basic form with no validation. Created beginning validation for first name only as a starting point.

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,10 +1,48 @@
+import { useState } from 'react';
+
 const BasicForm = (props) => {
+  const [firstName, setFirstName] = useState('');
+  const [firstNameIsTouched, setFirstNameIsTouched] = useState(false);
+
+  const firstNameIsValid = firstName.trim() !== '';
+  const firstNameHasError = !firstNameIsValid && firstNameIsTouched;
+
+  const firstNameChangeHandler = e => setFirstName(e.target.value);
+  const firstNameBlurHandler = e => setFirstNameIsTouched(true);
+
+  let formIsValid = false;
+
+  if (firstNameIsValid) {
+    formIsValid = true;
+  }
+
+  const formSubmitHandler = e => {
+    e.preventDefault();
+    console.log('inside form submit handler');
+
+    if (!firstNameIsValid) {
+      console.log('some field is invalid. stop submitting form');
+      return;
+    }
+
+    setFirstName('');
+    setFirstNameIsTouched(false);
+  };
+
+  const firstNameClasses = `form-control ${firstNameHasError && 'invalid'}`;
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='control-group'>
-        <div className='form-control'>
+        <div className={firstNameClasses}>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input
+            type='text'
+            id='name'
+            onChange={firstNameChangeHandler}
+            onBlur={firstNameBlurHandler}
+          />
+          {firstNameHasError && <p className='error-text'>Please enter a valid value.</p>}
         </div>
         <div className='form-control'>
           <label htmlFor='name'>Last Name</label>
