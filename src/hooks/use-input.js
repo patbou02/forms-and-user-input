9a8a8475ad50f4b0c1b44e29a667fd07233f91df
Patrick Boutin <patrick.boutin@roberthalf.com>
2022-11-23 16:45:58 -0800

    Switched back App to use SimpleInput and in custom hook useInput, added a useReducer for state management.

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,22 +1,65 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+const initialInputState = {
+  value: '',
+  isTouched: false
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === 'INPUT') {
+    return {
+      value: action.value,
+      isTouched: state.isTouched
+    };
+  }
+  if (action.type === 'BLUR') {
+    return {
+      isTouched: true,
+      value: state.value
+    };
+  }
+  if (action.type === 'RESET') {
+    return {
+      value: '',
+      isTouched: false
+    };
+  }
+  return initialInputState;
+};
 
 const useInput = (validateValue) => {
-  const [enteredValue, setEnteredValue] = useState('');
-  const [isTouched, setIsTouched] = useState(false);
+  const [inputState, dispatch] = useReducer(inputStateReducer, initialInputState);
 
-  const valueIsValid = validateValue(enteredValue);//enteredValue.trim() !== '';
-  const hasError = !valueIsValid && isTouched;
+  // const [enteredValue, setEnteredValue] = useState('');
+  // const [isTouched, setIsTouched] = useState(false);
 
-  const valueChangeHandler = e => setEnteredValue(e.target.value);
-  const inputBlurHandler = e => setIsTouched(true);
+  const valueIsValid = validateValue(inputState.value);//enteredValue.trim() !== '';
+  const hasError = !valueIsValid && inputState.isTouched;
+
+  const valueChangeHandler = e => {
+    dispatch({
+      type: 'INPUT',
+      value: e.target.value
+    });
+    // setEnteredValue(e.target.value);
+  };
+  const inputBlurHandler = e => {
+    dispatch({
+      type: 'BLUR'
+    });
+    // setIsTouched(true);
+  };
 
   const reset = () => {
-    setEnteredValue('');
-    setIsTouched(false);
+    dispatch({
+      type: 'RESET'
+    });
+    // setEnteredValue('');
+    // setIsTouched(false);
   };
 
   return {
-    value: enteredValue,
+    value: inputState.value,
     isValid: valueIsValid,
     hasError,
     valueChangeHandler,
